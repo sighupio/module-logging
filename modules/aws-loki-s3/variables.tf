4b0dedc382182bdebd9bf8aeb843d5af57ccbfdb
Luca De Carne <luca.decarne@reevo.it>
2026-06-18 18:12:17 +0200

    chore: add license header

--- modules/aws-loki-s3/variables.tf
+++ modules/aws-loki-s3/variables.tf
@@ -1,3 +1,7 @@
+# Copyright (c) 2017-present SIGHUP s.r.l All rights reserved.
+# Use of this source code is governed by a BSD-style
+# license that can be found in the LICENSE file.
+
 variable "bucket_name" {
   type        = string
   description = "The name of the dedicated S3 bucket to create for Loki storage"
